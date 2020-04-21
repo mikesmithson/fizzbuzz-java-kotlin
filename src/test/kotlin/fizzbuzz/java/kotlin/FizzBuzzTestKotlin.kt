3b7468117851e9789e8c08e740eabe8d252cba74
Mike Smithson <mikesmithso@gmail.com>
2020-04-20 20:09:36 -0400

    change test to a todo test
    Signed-off-by: Mike Smithson <mikesmithso@gmail.com>

--- src/test/kotlin/fizzbuzz/java/kotlin/FizzBuzzTestKotlin.kt
+++ src/test/kotlin/fizzbuzz/java/kotlin/FizzBuzzTestKotlin.kt
@@ -1,13 +1,10 @@
 package fizzbuzz.java.kotlin
 
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class FizzBuzzTestKotlin {
 
 	@Test
-	fun `you can put in a todo for a test that doesnt exist`() {
-		assertThat(System.currentTimeMillis()).isGreaterThan(0)
-	}
+	fun `you can put in a todo for a test that doesnt exist`(): Unit = TODO("I am not implemented!")
 }
 
