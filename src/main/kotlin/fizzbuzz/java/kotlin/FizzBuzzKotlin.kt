2a9cfd9c89a50693463ef74de6ad83e7e8e65093
Mike Smithson <mikesmithso@gmail.com>
2020-04-21 18:40:09 -0400

    adding main() function to print out solution from 1 to 100
    Signed-off-by: Mike Smithson <mikesmithso@gmail.com>

--- src/main/kotlin/fizzbuzz/java/kotlin/FizzBuzzKotlin.kt
+++ src/main/kotlin/fizzbuzz/java/kotlin/FizzBuzzKotlin.kt
@@ -3,8 +3,13 @@ package fizzbuzz.java.kotlin
 object FizzBuzzKotlin {
 	fun fizzBuzz(number: Int): String =
 			identityStrategy
-			.invoke(fizzStrategy
-			.invoke(buzzStrategy
-			.invoke(Pair(number, EMPTY))))
-			.second
+					.invoke(fizzStrategy
+							.invoke(buzzStrategy
+									.invoke(Pair(number, EMPTY))))
+					.second
+
+	@JvmStatic
+	fun main(args: Array<String>) {
+		(1..100).forEach { println(fizzBuzz(it)) }
+	}
 }
